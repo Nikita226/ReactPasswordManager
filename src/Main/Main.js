b639b5524da003c019aba3b6155eb1ce9a86e0ea
Nikita226 <85822089+Nikita226@users.noreply.github.com>
2021-07-11 16:31:38 +0530

    Update Main.js

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,23 +1,34 @@
 import React from 'react'
 
 export default function Main() {
+
     return (
         <div>
             <h1>Landing Page</h1>
             <br/>
             <br/>
-            <br/>
-            <br/>
-            <a href="/login">Login</a>
-            <br/>
-            <br/>
-            <a href="/signup">Sign Up</a>
-            <br/>
-            <br/>
+            <div>
+                <br/>
+                <br/>
+                <a href="/login">Login</a>
+                <br/>
+                <br/>
+                <a href="/signup">Sign Up</a>
+                <br/>
+                <br/>
+            </div>
             <a href="/createInstance">Create Secret</a>
             <br/>
             <br/>
             <a href="/AllInstances">All Secrets</a>
+            <br/>
+            <br/>
+            <a href="https://truptipatil04.github.io/passwordgenerator/">Password Generator</a>
+            <br/>
+            <br/>
+            <a href="https://mrunalk05.github.io/PasswordAnalyzer/">Password Analyzer</a>
+                        
         </div>
     )
 }
+
